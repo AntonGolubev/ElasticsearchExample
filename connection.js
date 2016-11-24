9f302c25dc13eddbe82573f3954be52ca4ff1c93
Anton <anton@zettabox.com>
2016-11-24 23:10:00 +0100

    packages

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ var elasticsearch=require('elasticsearch');
 
 var client = new elasticsearch.Client( {  
   hosts: [
-    'http://localhost:32769/',
+    'http://localhost:32771/',
     //'https://[username]:[password]@[server]:[port]/'
   ]
 });
